6b18714cb520ccd3c9501cf2f7dc78c901661ec8
Bruno27866 <bamaral2.git2023@gmail.com>
2025-06-01 11:54:58 -0400

    POST: update styles

--- aaron_appraisals/src/App.jsx
+++ aaron_appraisals/src/App.jsx
@@ -6,8 +6,8 @@ import styles from './app.module.css';
 export function App() {
   return (
     <div className={styles.mainContainer}>
+      <Navbar />
       <div className={styles.homeContainer}>
-        <Navbar />
         <video autoPlay loop muted playsInline className={styles.backgroundVideo}>
           <source src={video} type="video/mp4" />
         </video>
--- aaron_appraisals/src/components/home.jsx
+++ aaron_appraisals/src/components/home.jsx
@@ -1,9 +1,33 @@
 import styles from '../styles/home.module.css';
+import image from '../assets/pictures/fred.png';
 
 export function Home() {
   return (
-    <div className={styles.homeContainer}>
-      <h1>Home Section</h1>
-    </div>
+    <section id='home' className={`${styles.home} container`}>
+      <div className="row align-items-center">
+        {/* Left column with text content */}
+        <div className="col-md-6">
+          <div className={styles.content}>
+            <h4>WELCOME TO FJK REAL ESTATE LTD. BROKERAGE</h4>
+            <h1>Hi, I'm <span>Fred</span>, Real Estate Broker</h1>
+            <p>
+              FJK Real Estate LTD. Brokerage is an agency based out of Toronto, 
+              Ontario.  We specialize in real estate in Toronto and into the 
+              Greater Toronto Area.  As a real estate broker, owner and Broker of 
+              Record Fred Klonowski, has sold homes and land for over twenty 
+              years negotiating deals for his clients. Whether you are a buyer 
+              or a seller, please contact FJK Real Estate for their expertise.
+            </p>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+        {/* Right column with an image */}
+        <div className="col-md-6">
+          <div className={styles.image}>
+            <img src={image} alt="programmer" className="img-fluid" />
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
